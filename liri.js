e869c2285aff4ee9112e5108556ee331080c96e2
Raghav Shiv <Raghav@Raghavs-MacBook-Air.local>
2019-06-20 20:45:21 -0700

    Added Spotify Function

--- liri.js
+++ liri.js
@@ -4,3 +4,18 @@ var keys = require("./keys.js");
 var fs = require('fs');
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
+
+function runSpotify(input) {
+    spotify.search({ type: 'track', query: input, limit: 1 }, function (err, data) {
+        if (err) {
+            console.log('Error occurred: ' + err);
+        } else {
+            song = data.tracks.items[0];
+            console.log(
+                '\n\nArtist: ' + song.artists[0].name + '\nSong: ' + song.name
+                + '\nPreview Link: ' + song.preview_url + '\nAlbum: ' +
+                song.album.name + '\n\n'
+            );
+        }
+    });
+};
